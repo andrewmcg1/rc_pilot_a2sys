95bba447ced79ad416b698c770c4de3254d7dbf4
andrewmcg1 <greekgeek191@gmail.com>
2023-05-25 14:01:37 -0400

    gps fixes

--- include/mod/naza_gps.h
+++ include/mod/naza_gps.h
@@ -1,45 +1,36 @@
-/**
- * <naza_gps.h>
- *
- * @brief Data structurs and functions to obtain gps readings from DJI Naza V2 Gps
- *
- * Development for this module is based on <a
- * href="https://www.rcgroups.com/forums/showthread.php?1995704-DJI-NAZA-GPS-communication-protocol-NazaDecoder-Arduino-library"
- * target="_blank">this forum post.</a>
- *
- * @author Glen Haggin (ghaggin@umich.edu)
- *
- * @addtogroup NazaGps
- * @{
- */
-
 #ifndef __NAZA_GPS__
 #define __NAZA_GPS__
 
 #include <stdint.h>
 
-#include <coordinates.h>
-
-/**
- * @brief   Gps fix type
- */
-typedef enum
+enum _FixType
 {
     NO_FIX = 0,
     FIX_2D = 2,
     FIX_3D = 3,
-    FIX_DGPS = 4
-} fixType_t;
+    FIX_DGPS = 4,
+};
+typedef enum _FixType FixType;
 
-/**
- *  @brief      Naza gps data structure
- */
+enum _ParseState
+{
+    START_BYTE_1,
+    START_BYTE_2,
+    MESSAGE_ID,
+    VALIDATE_PAYLOAD,
+    RECIEVE_PAYLOAD,
+    CHECKSUM_1,
+    CHECKSUM_2,
+    DECODE_PAYLOAD
+};
+typedef enum _ParseState ParseState;
+
 typedef struct gps_data_t
 {
     lla_t lla;
     ned_waypoint_t ned;
     double spd;        ///< speed in m/s
-    fixType_t fix;     ///< fix type
+    FixType fix;     ///< fix type
     uint8_t sat;       ///< number of satellites
     double headingNc;  ///< heading (not tilt compensated) in degrees
     double cog;        ///< course over ground
@@ -59,29 +50,7 @@ typedef struct gps_data_t
 
 extern gps_data_t gps_data;
 
-/**
- * @brief   Set up serial connection to the naza gps unit
- *
- * The serial port is defined in the code to be the GPS (uart2) port on the beaglebone
- *
- * Usage: main.c, line 431
- *
- * @return  0 on success, -1 on failure (not good at detecting failure)
- */
 int gps_init();
-
-/**
- * @brief   attempt to extract data from naza gps
- *
- * Function attempts to read one byte at a time from the naza gps unit
- * until all bytes are read.  The data is parsed one byte at a time.
- *
- * Usage: main.c, line 155
- *
- * @return  0 on success, -1 on failure
- */
 int gps_getData();
 
-#endif /* __NAZA_GPS__ */
-
-/* @} end group NazaGps */
+#endif
--- src/mod/naza_gps.c
+++ src/mod/naza_gps.c
@@ -9,40 +9,90 @@
 #include <unistd.h>
 
 #include <rc/time.h>
+#include <rc/uart.h>
+
 
 #include <naza_gps.h>
-#include <serial_com.h>
 #include <state_estimator.h>
 
+#define BAUDRATE    115200
+#define TIMEOUT_S   .5
+
+#define GPS_BUS 2
+
 #define PAYLOAD_SIZE 64
+#define GPS_PAYLOAD_LENGTH 58
+#define COMPASS_PAYLOAD_LENGTH 6
+
 #define GPS_STARTBYTE1 0x55       ///< first start byte
 #define GPS_STARTBYTE2 0xAA       ///< second start byte
 #define PAYLOAD_GPS 0x10          ///< gps message byte
-#define PAYLOAD_GPS_LEN 0x3A      ///< gps message length
 #define PAYLOAD_COMPASS 0x20      ///< compass message byte
-#define PAYLOAD_COMPASS_LEN 0x06  ///< compass message length
 
-unsigned char payload[PAYLOAD_SIZE];  // TODO: static? for each of these variables...
-unsigned char cs1, cs2;               ///< checksum 1 and 2
-int msgId, msgLen;
-int gps_portID;
+/**
+ * Structure to store raw gps data as individual segments of data
+ */
+typedef struct RawGPSData
+{
+    long int dateAndTime;
+    long int longitude;
+    long int latitude;
+    long int altitude;
+    long int horrizontalAccuracyEstimate;
+    long int verticalAccuracyEstimate;
+    long int unknown1;
+    long int NEDNorthVelocity;
+    long int NEDEashVelocity;
+    long int NEDDownVelocity;
+    short int positionDOP;
+    short int verticalDOP;
+    short int northernDOP;
+    short int easternDOP;
+    unsigned char numberOfSatelites;
+    unsigned char unknown2;
+    unsigned char fixType;
+    unsigned char unknown3;
+    unsigned char fixStatusFlag;
+    short int unknown4;
+    unsigned char xorMask;
+    short int sequenceNumber;
+} RawGPSData;
+
+/**
+ * Struct to store raw compass data
+ */
+typedef struct RawCompassData
+{
+    short int x;
+    short int y;
+    short int z;
+} RawCompassData;
+
+/**
+ * This union is used to quickly convert between raw data structs and uint8_t arrays
+ */
+typedef union struct_to_int
+{
+    RawGPSData gps_structure;
+    RawCompassData compass_structure;
+    uint8_t struct_as_int[PAYLOAD_SIZE];
+} struct_to_int;
 
 gps_data_t gps_data;
+
+RawGPSData gps_data_raw;
+RawCompassData compass_data_raw;
 
 /**
  * Functions only to be used locally
  */
 static int __gps_parse(const int input);
 static void __updateCS(const int input);
-static void __gps_decode();
-static int32_t __decodeLong(uint8_t idx, uint8_t mask);
-static int16_t __decodeShort(uint8_t idx, uint8_t mask);
+static void __gps_decode(unsigned char messageID);
 
 int gps_init()
 {
-    int baudRate = 115200;
-    gps_portID = serial_open("/dev/ttyS2", baudRate, 0);  // TODO: Add serial port to settings file
-    if (gps_portID == -1)
+    if (rc_uart_init(GPS_BUS, BAUDRATE, TIMEOUT_S, 0, 1, 0))
     {
         printf("Failed to open Serial Port\n");
         return -1;
@@ -57,9 +107,10 @@ int gps_init()
 int gps_getData()
 {
     unsigned char buffer;
-
-    while (read(gps_portID, &buffer, 1) > 0)
+    if(rc_uart_bytes_available(GPS_BUS))
     {
+
+        rc_uart_read_bytes(GPS_BUS, &buffer, 1);
         __gps_parse(buffer);
     }
 
@@ -78,181 +129,209 @@ int gps_getData()
 
 static int __gps_parse(const int input)
 {
-    static int seq = 0, cnt = 0;
+    static int count;
+    static uint8_t checkSum[2];
+    static ParseState parseState = START_BYTE_1;
+    static unsigned char messageID;
+    static unsigned char messageLength;
+    static unsigned char payload[PAYLOAD_SIZE];
 
-    if (seq == 0 && input == GPS_STARTBYTE1)
+    switch (parseState)
     {
-        ++seq;
-    }
-    else if (seq == 1 && input == GPS_STARTBYTE2)
-    {
-        cs1 = 0;
-        cs2 = 0;
-        ++seq;
-    }
-    else if (seq == 2)
-    {
-        msgId = input;
-        __updateCS(input);
-        ++seq;
-    }
-    else if (seq == 3 && ((msgId == PAYLOAD_GPS && input == PAYLOAD_GPS_LEN) ||
-                             (msgId == PAYLOAD_COMPASS && input == PAYLOAD_COMPASS_LEN)))
-    {
-        msgLen = input;
-        cnt = 0;
-        __updateCS(input);
-        ++seq;
-    }
-    else if (seq == 4)
-    {
-        payload[cnt++] = input;
-        __updateCS(input);
-        if (cnt >= msgLen)
+    case START_BYTE_1:
+        if (input == GPS_STARTBYTE1)
+            parseState = START_BYTE_2;
+        break;
+
+    case START_BYTE_2:
+        if (input == GPS_STARTBYTE2)
         {
-            ++seq;
+            parseState = MESSAGE_ID;
+            checkSum[0] = 0;
+            checkSum[1] = 0;
         }
-    }
-    else if (seq == 5 && input == cs1)
-    {
-        ++seq;
-    }
-    else if (seq == 6 && input == cs2)
-    {
-        ++seq;
-    }
-    else
-    {
-        seq = 0;
-        return -1;
+        else
+            parseState = START_BYTE_1;
+        break;
+
+    case MESSAGE_ID:
+        messageID = input;
+        checkSum[0] += input;
+        checkSum[1] += checkSum[0];
+        parseState = VALIDATE_PAYLOAD;
+        break;
+
+    case VALIDATE_PAYLOAD:
+        if ((messageID == PAYLOAD_GPS && input == GPS_PAYLOAD_LENGTH)
+           || messageID == PAYLOAD_COMPASS && input == COMPASS_PAYLOAD_LENGTH)
+        {
+            messageLength = input;
+            count = 0;
+            checkSum[0] += input;
+            checkSum[1] += checkSum[0];
+            parseState = RECIEVE_PAYLOAD;
+        }
+        else
+            parseState = START_BYTE_1;
+        break;
+
+    case RECIEVE_PAYLOAD:
+        payload[count++] = input;
+        checkSum[0] += input;
+        checkSum[1] += checkSum[0];
+        if (count >= messageLength)
+        {
+            parseState = CHECKSUM_1;
+        }
+        break;
+
+    case CHECKSUM_1:
+        if (input == checkSum[0])
+            parseState = CHECKSUM_2;
+        else
+            parseState = START_BYTE_1;
+        break;
+
+    case CHECKSUM_2:
+        if (input == checkSum[1])
+            parseState = DECODE_PAYLOAD;
+        else
+            parseState = START_BYTE_1;
+        break;
+
+    case DECODE_PAYLOAD:
+        if (messageID == PAYLOAD_GPS)
+        {
+            gps_data_raw = *(RawGPSData*)payload;
+        }
+        else if (messageID == PAYLOAD_COMPASS)
+        {
+            compass_data_raw = *(RawCompassData*)payload;
+        }
+        parseState = START_BYTE_1;
+        __gps_decode(messageID);
     }
 
-    // Full message read into buffer, both checksums good
-    if (seq == 7)
-    {
-        seq = 0;
-        __gps_decode();
-    }
     return 0;
 }
 
-static void __updateCS(const int input)
+static void __gps_decode(unsigned char messageID)
 {
-    cs1 += input;
-    cs2 += cs1;
-}
-
-static void __gps_decode()
-{
+    struct_to_int gps_union;
     static int16_t magXMax, magXMin, magYMax, magYMin;
+    uint8_t mask = 0;
+    uint8_t mask_bits[8];
 
-    if (msgId == PAYLOAD_GPS)
+    if (messageID == PAYLOAD_GPS)
     {
-        uint8_t mask = payload[55];
-        uint32_t time = __decodeLong(0, mask);
-        gps_data.second = time & 0b00111111;
-        time >>= 6;
-        gps_data.minute = time & 0b00111111;
-        time >>= 6;
-        gps_data.hour = time & 0b00001111;
-        time >>= 4;
-        gps_data.day = time & 0b00011111;
-        time >>= 5;
-        if (gps_data.hour > 7) gps_data.day++;
-        gps_data.month = time & 0b00001111;
-        time >>= 4;
-        gps_data.year = time & 0b01111111;
+        mask = gps_data_raw.xorMask;
 
-        gps_data.lla.lon = (double)__decodeLong(4, mask) / 10000000;
-        gps_data.lla.lat = (double)__decodeLong(8, mask) / 10000000;
-        gps_data.lla.alt = (double)__decodeLong(12, mask) / 1000;
+        gps_union.gps_structure = gps_data_raw;
 
-        double nVel = (double)__decodeLong(28, mask) / 100;
-        double eVel = (double)__decodeLong(32, mask) / 100;
-        gps_data.spd = sqrt(nVel * nVel + eVel * eVel);
-        gps_data.cog = atan2(eVel, nVel) * 180.0 / M_PI;
-        if (gps_data.cog < 0) gps_data.cog += 360.0;
-        gps_data.gpsVsi = -(double)__decodeLong(36, mask) / 100;
-        gps_data.vdop = (double)__decodeShort(42, mask) / 100;
-        double ndop = (double)__decodeShort(44, mask) / 100;
-        double edop = (double)__decodeShort(46, mask) / 100;
-        gps_data.hdop = sqrt(ndop * ndop + edop * edop);
-        gps_data.sat = payload[48];
-        uint8_t fixType = payload[50] ^ mask;
-        uint8_t fixFlags = payload[52] ^ mask;
-        switch (fixType)
-        {
-            case 2:
-                gps_data.fix = FIX_2D;
-                break;
-            case 3:
-                gps_data.fix = FIX_3D;
-                break;
-            default:
-                gps_data.fix = NO_FIX;
-                break;
-        }
-        if ((gps_data.fix != NO_FIX) && (fixFlags & 0x02))
-        {
-            gps_data.fix = FIX_DGPS;
-        }
+        for(int i = 0; i < GPS_PAYLOAD_LENGTH; i++)
+            gps_union.struct_as_int[i] ^= mask;
 
-        // Convert current location to north east down coordinates;
-        gps_data.ned = lla2ned(&gps_data.lla);
+        gps_data_raw = gps_union.gps_structure;
 
-        // Log time that data is received
-        gps_data.gps_data_received_ns = rc_nanos_since_epoch();
+        gps_data_raw.numberOfSatelites ^= mask;
+        gps_data_raw.unknown2 ^= mask;
+        gps_data_raw.sequenceNumber ^= (mask << 8) & mask;
 
-        // Check if gps is valid (> 4 satelites for 3D)
-        gps_data.gps_valid = (gps_data.sat >= 4);
     }
-    else if (msgId == PAYLOAD_COMPASS)
+    else if (messageID == PAYLOAD_COMPASS)
     {
-        uint8_t mask = payload[4];
-        mask = (((mask ^ (mask >> 4)) & 0x0F) | ((mask << 3) & 0xF0)) ^
-               (((mask & 0x01) << 3) | ((mask & 0x01) << 7));
-        int16_t x = __decodeShort(0, mask);
-        int16_t y = __decodeShort(2, mask);
-        if (x > magXMax) magXMax = x;
-        if (x < magXMin) magXMin = x;
-        if (y > magYMax) magYMax = y;
-        if (y < magYMin) magYMin = y;
-        gps_data.headingNc =
-            -atan2(y - ((magYMax + magYMin) / 2), x - ((magXMax + magXMin) / 2)) * 180.0 / M_PI;
-        if (gps_data.headingNc < 0)
-        {
-            gps_data.headingNc += 360.0;
-        }
+        mask = compass_data_raw.z_vector & 0x00ff;
+		mask = (((mask ^ (mask >> 4)) & 0x0F) | ((mask << 3) & 0xF0)) ^ (((mask & 0x01) << 3) | ((mask & 0x01) << 7));
+
+        gps_union.compass_structure = compass_data_raw;
+
+        for(int i = 0; i < GPS_PAYLOAD_LENGTH; i++)
+            gps_union.struct_as_int[i] ^= mask;
+
+        compass_data_raw = gps_union.compass_structure;
+
+        compass_data_raw.z_vector ^= mask;
     }
     return;
 }
 
-static int32_t __decodeLong(uint8_t idx, uint8_t mask)
+int convert_gps_raw_to_final()
 {
-    union {
-        uint32_t l;
-        uint8_t b[4];
-    } val;
+    uint32_t time = gps_data_raw.dateAndTime;
 
-    for (int i = 0; i < 4; i++)
-    {
-        val.b[i] = payload[idx + i] ^ mask;
-    }
+    gps_data.second = time & 0b00111111;
+    time >>= 6;
+    gps_data.minute = time & 0b00111111;
+    time >>= 6;
+    gps_data.hour = time & 0b00001111;
+    time >>= 4;
+    gps_data.day = time & 0b00011111;
+    time >>= 5;
+    if (gps_data.hour > 7) 
+        gps_data.day++;
+    gps_data.month = time & 0b00001111;
+    time >>= 4;
+    gps_data.year = time & 0b01111111;
 
-    return val.l;
+
+    gps_data.lla.lat = (double)gps_data_raw.latitude / 10000000.0;
+    gps_data.lla.lon = (double)gps_data_raw.longitude / 10000000.0;
+    gps_data.lla.alt = (double)gps_data_raw.altitude / 1000.0;
+
+
+    double Nvel = (double)gps_data_raw.NEDNorthVelocity / 100.0;
+    double Evel = (double)gps_data_raw.NEDEastVelocity / 100.0;
+    gps_data.spd = sqrt(nVel * nVel + eVel * eVel);
+
+
+    gps_data.cog = atan2(eVel, nVel);
+    if (gps_data.cog < 0) 
+        gps_data.cog += 360.0;
+
+
+    gps_data.gpsVsi = -(double)gps_data_raw.NEDDownVelocity / 100.0;
+
+    gps_data.vdop = (double)gps_data_raw.verticalDOP / 100.0;
+
+    double ndop = (double)gps_data_raw.northernDOP / 100.0;
+    double edop = (double)gps_data_raw.easternDOP / 100.0;
+    gps_data.hdop = sqrt(ndop * ndop + edop * edop);
+
+    gps_data.sat = dps_data_raw.numberOfSatelites;
+
+
+    if(gps_data_raw.fixStatusFlag == 0x02 && gps_data_raw.fixType != NO_FIX)
+        gps_data.fix = FIX_DGPS;
+    else
+        gps_data.fix = gps_data_raw.fixType;
+
+
+    // Convert current location to north east down coordinates;
+    gps_data.ned = lla2ned(&gps_data.lla);
+
+    // Log time that data is received
+    gps_data.gps_data_received_ns = rc_nanos_since_epoch();
+
+    // Check if gps is valid (> 4 satelites for 3D)
+    gps_data.gps_valid = (gps_data.sat >= 4);
+
+
+
+    // Compass data
+    static int16_t magXMax, magXMin, magYMax, magYMin;
+
+    if (compass_data_raw.x > magXMax) 
+        magXMax = x;
+    if (compass_data_raw.x < magXMin) 
+        magXMin = x;
+    if (compass_data_raw.y > magYMax) 
+        magYMax = y;
+    if (compass_data_raw.y < magYMin) 
+        magYMin = y;
+    gps_data.headingNc = -atan2(compass_data_raw.y - ((magYMax + magYMin) / 2), 
+                                compass_data_raw.x - ((magXMax + magXMin) / 2)) * 180.0 / M_PI;
+    if (gps_data.headingNc < 0)
+        gps_data.headingNc += 360.0;
+
 }
 
-static int16_t __decodeShort(uint8_t idx, uint8_t mask)
-{
-    union {
-        uint16_t s;
-        uint8_t b[2];
-    } val;
-
-    for (int i = 0; i < 2; i++)
-    {
-        val.b[i] = payload[idx + i] ^ mask;
-    }
-
-    return val.s;
-}
